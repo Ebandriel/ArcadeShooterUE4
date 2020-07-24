3768bef3c8a38d25e26d8e00ea953ded758bf01f
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-07-24 22:36:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArcadeShooter
+
+Developed with Unreal Engine 4
---
+++ Source/ArcadeShooter/ArcadeShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcadeShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADESHOOTER_API AArcadeShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
