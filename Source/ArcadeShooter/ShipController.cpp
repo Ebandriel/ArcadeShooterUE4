3e54eb6432b3311ee6c9eb06df20c3eaefb373d8
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-07-24 23:40:20 +0100

    AddedPlayer controll

--- Source/ArcadeShooter/ShipController.cpp
+++ Source/ArcadeShooter/ShipController.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "ArcadeShooter.h"
 #include "ShipController.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AShipController::AShipController()
@@ -9,6 +11,9 @@ AShipController::AShipController()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +27,12 @@ void AShipController::BeginPlay()
 void AShipController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (!CurrentVelocity.IsZero())
+	{
+		FVector NewLocation = GetActorLocation() +
+			Speed * CurrentVelocity * DeltaTime;
+		SetActorLocation(NewLocation);
+	}
 
 }
 
@@ -29,6 +40,16 @@ void AShipController::Tick(float DeltaTime)
 void AShipController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent->BindAxis("MoveX", this, &AShipController::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AShipController::Move_YAxis);
+}
 
+void AShipController::Move_XAxis(float AxisValue)
+{
+	CurrentVelocity.X = AxisValue * 100.0f;
+}
+void AShipController::Move_YAxis(float AxisValue)
+{
+	CurrentVelocity.Y = AxisValue * 100.0f;
 }
 
--- Source/ArcadeShooter/ShipController.h
+++ Source/ArcadeShooter/ShipController.h
@@ -26,4 +26,16 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere)
+	UShapeComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 10.0f;
+
+	void Move_XAxis(float AxisValue);
+	void Move_YAxis(float AxisValue);
+	void Fire();
+
+	FVector CurrentVelocity;
+
 };
